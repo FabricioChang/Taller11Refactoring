7ed38cf33718eb2d0697f8f772c09b5787650919
StevenMiraba <wsmiraba@espol.edu.ec>
2024-08-07 22:19:00 -0500

    Preserve whole object

--- src/main/java/espol/edu/ec/taller11refactoring/SistemaAtencionMedico.java
+++ src/main/java/espol/edu/ec/taller11refactoring/SistemaAtencionMedico.java
@@ -21,29 +21,40 @@ public class SistemaAtencionMedico {
         pacientes.add(paciente);
     }
 
+    public void removePaciente(Paciente paciente) {
+        pacientes.remove(paciente);
+    }
+
     public void agregarMedico(Medico medico) {
         medicos.add(medico);
+    }
+
+    public void removeMedico(Medico medico) {
+        medicos.remove(medico);
     }
 
     public void agregarServicioMedico(ServicioMedico servicioMedico) {
         serviciosMedicos.add(servicioMedico);
     }
 
-    public void agendarConsulta(Paciente paciente, Consulta consulta){
-        double costoConsulta = consulta.getServicioMedico().getCosto();
-        int edadPaciente = paciente.getEdad();
-        costoConsulta = calcularValorFinalConsulta(costoConsulta,edadPaciente);
-        System.out.println("Se han cobrado "+ costoConsulta+ " dolares de su tarjeta de credito");
-        paciente.historialMedico.getConsultas().add(consulta);
+    public void removeServicioMedico(ServicioMedico servicioMedico) {
+        serviciosMedicos.remove(servicioMedico);
     }
 
-    public double calcularValorFinalConsulta(double costoConsulta, int edadPaciente){
+    public void agendarConsulta(Paciente paciente, Consulta consulta) {
+        double costoConsulta = calcularValorFinalConsulta(consulta);
+        System.out.println("Se han cobrado " + costoConsulta + " dolares de su tarjeta de credito");
+        paciente.getHistorialMedico().getConsultas().add(consulta);
+    }
+
+    public double calcularValorFinalConsulta(Consulta consulta) {
+        double costoConsulta = consulta.getServicioMedico().getCosto();
+        int edadPaciente = consulta.getPaciente().getEdad();
         double valorARestar = 0;
-        
-        if(edadPaciente>=edad_de_tercera_edad){
-            valorARestar = costoConsulta*descuento_de_adultos_mayores;
+        if (edadPaciente >= 65) {
+            valorARestar = costoConsulta * 0.25; // 0.25 es el descuento para adultos mayores
         }
-        return costoConsulta-valorARestar;
+        return costoConsulta - valorARestar;
     }
 
     public <T> T obtenerElemento(List<T> elementos, Predicate<T> criterio) {
